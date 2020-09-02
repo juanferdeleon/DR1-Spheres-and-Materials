4f83007ad07a627f59ba7b2505428e91422fe0f5
juanferdeleon <dele17822@uvg.edu.gt>
2020-09-02 15:26:21 -0600

    Output

--- Raytracer.py
+++ Raytracer.py
@@ -16,18 +16,41 @@ from sphere import Sphere, Material
 if __name__ == '__main__':
     '''Main Program'''
 
-    brick = Material(diffuse = color(204, 64, 64 ))
-    stone = Material(diffuse = color(112, 112, 112 ))
-    grass = Material(diffuse = color(128, 255, 0))
+    snow = Material(diffuse = color(224, 224, 224))
+    buttons = Material(diffuse = color(112, 112, 112 ))
+    smile = Material(diffuse = color(0, 0, 0))
+    carrot = Material(diffuse = color(255, 92, 57))
+    eye = Material(diffuse = color(153, 153, 153))
 
 
     width = 500
     height = 300
     r = Raytracer(width,height)
 
-    r.scene.append( Sphere(V3(0, 0,  -7), 1, brick) )
-    r.scene.append( Sphere(V3(1, 1, -10), 1, stone) )
-    r.scene.append( Sphere(V3(-1.5, -1.5, -13), 1, grass) )
+    # BODY
+    r.scene.append( Sphere(V3(0, 0.80, -5), 0.60, snow) ) #HEAD
+    r.scene.append( Sphere(V3(0, 0,  -5), 0.75, snow) ) # BELLY
+    r.scene.append( Sphere(V3(0, -1, -5), 1, snow) ) # LEGS
+
+    # Buttons
+    r.scene.append( Sphere(V3(0, 0, -2), 0.05, buttons) )
+    r.scene.append( Sphere(V3(0, -0.25, -2), 0.05, buttons) )
+    r.scene.append( Sphere(V3(0, -0.5, -2), 0.05, buttons) )
+
+    # Smile
+    r.scene.append( Sphere(V3(0.045, 0.25, -2), 0.02, smile) )
+    r.scene.append( Sphere(V3(0.1, 0.30, -2), 0.02, smile) )
+    r.scene.append( Sphere(V3(-0.045, 0.25, -2), 0.02, smile) )
+    r.scene.append( Sphere(V3(-0.1, 0.30, -2), 0.02, smile) )
+
+    # Nose
+    r.scene.append( Sphere(V3(0, 0.32, -2), 0.02, carrot) )
+
+    # Eyes
+    r.scene.append( Sphere(V3(0.075, 0.4, -2), 0.04, eye) )
+    r.scene.append( Sphere(V3(-0.075, 0.4, -2), 0.04, eye) )
+
+
     
     r.rtRender()
 
